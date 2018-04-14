905066d5db0413afaa7b2f6c8678a24c78df92fc
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-04-15 04:02:50 +0900

    Artworkエンティティを追加

---
+++ ios-itunes-search-app/Artwork.swift
@@ -0,0 +1,23 @@
+//
+//  Artwork.swift
+//  ios-itunes-search-app
+//
+//  Created by yogasawara@stv on 2018/04/15.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import UIKit
+import RepositoryTool
+
+struct Artwork: RepositoryElement {
+    typealias Identifier = Int
+    let id: Int
+    let url: String
+    let image: UIImage?
+    
+    init(from dto: ArtworkDto) {
+        self.id = dto.id
+        self.url = dto.url
+        self.image = dto.image
+    }
+}
