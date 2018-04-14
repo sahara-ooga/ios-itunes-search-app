28a9ceb6063660c5ca3d3e0e8d3b2260579cecb4
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-04-15 02:58:21 +0900

    import carthage

---
+++ ios-itunes-search-app/ArtworkRepository.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  ios-itunes-search-app
+//
+//  Created by yogasawara@stv on 2018/04/15.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import RepositoryTool
+
+class ArtworkRepository {
+    
+}
