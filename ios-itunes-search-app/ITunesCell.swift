5e6436b861c7b11e24c372058dc9b07b981280b2
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-04-15 16:28:23 +0900

    Add TableView Cell

---
+++ ios-itunes-search-app/ITunesCell.swift
@@ -0,0 +1,30 @@
+//
+//  ITunesCell.swift
+//  ios-itunes-search-app
+//
+//  Created by yogasawara@stv on 2018/04/15.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ITunesCell: UITableViewCell {
+    static var identifier: String {
+        get {
+            return String(describing: self)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+
+}
