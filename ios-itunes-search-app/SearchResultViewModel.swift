a53173713084f72ffea81f1516e177b5ce8652d3
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-04-15 18:18:16 +0900

    Add Storyboard and ViewModel

---
+++ ios-itunes-search-app/SearchResultViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchResultViewModel.swift
+//  ios-itunes-search-app
+//
+//  Created by yogasawara@stv on 2018/04/15.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import Foundation
